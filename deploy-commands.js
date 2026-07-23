4b1d35be0ba4a43c8eacf3aa4eea900fa9a33951
AntiGod-EL <qiupediastore@gmail.com>
2026-07-23 14:40:17 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,50 @@
+require("dotenv").config();
 
+const fs = require("fs");
+const path = require("path");
+
+const { REST, Routes } = require("discord.js");
+
+const commands = [];
+
+const commandFolders = fs.readdirSync("./src/commands");
+
+for (const folder of commandFolders) {
+
+    const commandFiles = fs
+        .readdirSync(path.join("./src/commands", folder))
+        .filter(file => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+
+        const command = require(path.join(process.cwd(), "src", "commands", folder, file));
+
+        commands.push(command.data.toJSON());
+
+    }
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+
+    try {
+
+        console.log("Deploying slash commands...");
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            {
+                body: commands
+            }
+        );
+
+        console.log("✅ Slash commands deployed!");
+
+    } catch (err) {
+
+        console.error(err);
+
+    }
+
+})();
