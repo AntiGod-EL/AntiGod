234fbc3e48faa3efbe32fe86ed95f33ed102257f
AntiGod-EL <qiupediastore@gmail.com>
2026-07-23 14:26:47 +0700

    Set up Discord bot with MongoDB connection

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+require("dotenv").config();
 
+const {
+    Client,
+    GatewayIntentBits,
+    Collection
+} = require("discord.js");
+
+const mongoose = require("mongoose");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.commands = new Collection();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(console.error);
+
+require("./src/handlers/eventHandler")(client);
+require("./src/handlers/commandHandler")(client);
+
+client.login(process.env.TOKEN);
