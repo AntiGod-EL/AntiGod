c633d5f48f161f14c95351c2f599fa47cceb1586
AntiGod-EL <qiupediastore@gmail.com>
2026-07-23 14:37:07 +0700

    Add ClientReady event handler for logging and activity

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,1 +1,14 @@
+const { Events } = require("discord.js");
 
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+
+    execute(client) {
+
+        console.log(`🤖 Logged in as ${client.user.tag}`);
+
+        client.user.setActivity("AntiGod Economy");
+
+    }
+};
